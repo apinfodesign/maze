f83887ad7e917aeaada94e706e819f8b35c14633
My Name <me@example.com>
2015-01-13 11:25:05 -0800

    documentation edits and comments

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var gamelib = require("jsi-gamelib");
 var gameMapIn = require("./gamemap.json")
+// bring in jsi-gamelib via gamelib above
+
 console.log(gameMapIn.rooms);
 
 var currentRoom;  // arbitrary starting room for user
-var newCurrentRoom;
+var newCurrentRoom;  // when room change is possible this will assume new room value
 
 // set up user interface
 var readline = require('readline');
@@ -12,11 +14,11 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-// bring in jsi-gamelib via gamelib above
 // log the map in gamelib
 function outputMaze(){
-console.log("");
-console.log(gamelib.map(require(process.argv[2])));
+ console.log("");
+ console.log(gamelib.map(require(process.argv[2])));
+ console.log("");
 };
 
 console.log("Welcome to the maze.");
@@ -36,15 +38,14 @@ console.log("Welcome to the maze.");
 //				2 giveUserCurrentPosition()
 /////////////////////////////////////////////
 
-giveUserCurrentStatus("B");  //assign start status here
+//toop level function call.  Calls all other functions.
+giveUserCurrentStatus("B");  //assign start status here 
 
 function giveUserCurrentStatus(arg){
 	var currentRoom = arg;    
 //	console.log(gamelib.map(require(process.argv[2])));
-	outputMaze();
-
- 	console.log("You are in room: " + currentRoom);
-
+	outputMaze();   //display maze every time user gets status update
+ 	console.log("You are in room: " + currentRoom);  
 	evaluateCurrentPositionForAlerts(currentRoom);   //seems to work!
 };	
 
@@ -52,31 +53,27 @@ function evaluateCurrentPositionForAlerts(arg){
 	 currentRoom = arg;
      for (i=0; i< gameMapIn.rooms.length; i++){
         	if  ((gameMapIn.rooms[i].treasure === true) && (gameMapIn.rooms[i].name === currentRoom))
-        		{ 
-         		console.log("YOU FOUND THE TREASURE!  O ! M ! G ! YOU WIN! ");   
-        		};
-			};
-	//console.log("evaluating current position for alerts");
+        		{console.log("YOU FOUND THE TREASURE!  O ! M ! G ! YOU WIN! ");  };
+			}; 
 	askUserNextMove(currentRoom);
 };
 
 function askUserNextMove(argCurrentRoom){
-    rl.question("Which direction to move next? ( N S E W ) ", function(argNextMove) {
+   rl.question("Which direction to move next? ( N S E W ) ", function(argNextMove) {
    console.log("You chose direction: ", argNextMove + " from currentRoom: "+argCurrentRoom );
-   //rl.close();
- 
-   checkMovePossible(argNextMove, argCurrentRoom);
+   //rl.close();   //not necessary to close read line
+    checkMovePossible(argNextMove, argCurrentRoom);   //check if requested move is possible
 	});
 };
 
 function checkMovePossible(arg1, arg2){
 	//take current location and figure out if requested move possible
-	//console.log("here we check if current request is possible.... ")
-	answer = arg1;
-	currentRoom = arg2;
+ 	answer = arg1;   //user chosen direction
+	currentRoom = arg2;  //user current location passed in 
 	//console.log("entering function <checkMovePossible> answer we are checking is "+ answer);
 	//console.log("currentRoom is " + currentRoom +"  and newCurrentRoom" + newCurrentRoom);
-	switch(answer) 
+	
+	switch(answer)    //based on user chosen direction do one of four things:
 	{
     case "N":
     	//console.log("you chose N");
@@ -85,9 +82,10 @@ function checkMovePossible(arg1, arg2){
         		{ 
          		var newCurrentRoom = gameMapIn.rooms[i].north;  
   			      	giveUserCurrentStatus(newCurrentRoom);     		
-       		} 
-        	else{
-        		//console.log(answer + " is not a doable direction");
+       			} 
+        	else
+        		{
+        		//console.log(answer + " is not a doable direction");  //not sure why this seemed to be necessary.
          		};
 			};
  			console.log("No NORTH move was possible");
@@ -104,7 +102,7 @@ function checkMovePossible(arg1, arg2){
         			}
 			else{
         		//console.log(answer + " is not a doable direction");
-         	};  
+ 	        	};  
 			}; //END FOR LOOP
  			console.log("No SOUTH move was possible");
     		giveUserCurrentStatus(currentRoom);     		
@@ -117,9 +115,10 @@ function checkMovePossible(arg1, arg2){
         		{ var newCurrentRoom = gameMapIn.rooms[i].east;         		
 				giveUserCurrentStatus(newCurrentRoom);
         		}  
-			else{
+			else
+				{
         		//console.log(answer + " is not a doable direction");
-         	};
+         		};
 			}; //END FOR LOOP
  			console.log("No EAST move was possible");
     		giveUserCurrentStatus(currentRoom);     		
@@ -133,20 +132,19 @@ function checkMovePossible(arg1, arg2){
         		{ var newCurrentRoom = gameMapIn.rooms[i].west; 
         		giveUserCurrentStatus(newCurrentRoom);
 	      		}
-			else{ //console.log(answer + " is not a doable direction"); 
-			};
-			
+			else
+				{ //console.log(answer + " is not a doable direction"); 
+				};
 			}; //END FOR LOOP
-
  			console.log("No WEST move was possible.");
     		giveUserCurrentStatus(currentRoom);     		
-    	
-         break;
+        break;
 
     default:
 		//console.log(" Sorry, you must chose N E S or W.")
         //console.log( "You have hit default and this is your current room:  "+ currentRoom);
-        denyOrUpdate();
+
+        denyOrUpdate();   //if user chooses invalid input send error and call main function again.
  	}
 //	console.log("currentRoom is: "+ currentRoom + "    newCurrentRoom is " + newCurrentRoom );
 	
@@ -154,9 +152,7 @@ function checkMovePossible(arg1, arg2){
 
 function denyOrUpdate(){
    console.log(" Sorry, you must chose N E S or W.")
-    giveUserCurrentStatus(currentRoom);
-
- 
+   giveUserCurrentStatus(currentRoom);
 };
 
 
